4936b4ede72c1d93ab9901df039e70f727d9f7b6
JT Turner <jtwebman@gmail.com>
2024-11-03 22:26:01 -0800

    Add in React web app and vite-node for server.

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,25 @@
+import config from '@tasky/eslint-config';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  ...[].concat(config),
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+];
